4dae1f8db2363f7ef853fe3b34f1cf29529103e8
Noyon141 <mdnoyon1590852@gmail.com>
2025-09-08 03:50:37 -0700

    feat: Simplify database connection handling by removing status message

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,10 @@ import HomeContent from "@/components/home-content";
 import { testDB } from "@/lib/test-db";
 
 export default async function Home() {
-  const dbTime = await testDB();
+  await testDB();
 
   return (
     <>
-      <p>
-        {dbTime
-          ? `✅ Database Connected. Current DB Time: ${dbTime}`
-          : "❌ Database not connected."}
-      </p>
       <HomeContent />
     </>
   );
